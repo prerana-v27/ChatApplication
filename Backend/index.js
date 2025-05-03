052721709a83ca45c15dc61960c12aa8654f1fe6
1DS22CB038PRERANAV <136422031+1DS22CB038PRERANAV@users.noreply.github.com>
2025-05-03 14:45:07 +0530

    changed index .js

--- Backend/index.js
+++ Backend/index.js
@@ -1,32 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import userRoute from "./route/user.route.js";
-
-
-// const app = express();
-// dotenv.config();
-
-// app.use(express.json());
-
-// const PORT = process.env.PORT || 5001;
-// const URI = process.env.MONGODB_URI;
-
-
-// try {
-//     mongoose.connect(URI);
-//     console.log("MongoDB Connected");
-// } catch (error) {
-//     console.error("MongoDB Connection erroe",error.message);     
-// }
-
-// app.use("/user",userRoute);
-
-// app.listen(PORT, () => {
-//   console.log(`Server is Running on port ${PORT}`)
-// });
-
-
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
